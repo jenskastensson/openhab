d0bafae50555974f85178e27c7b6f0aaf6b49e28
jenskastensson <jens@kastensson.se>
2014-12-20 14:23:15 +0100

    Add habmin

---
+++ distribution/openhabhome/copy_addons.bat
@@ -0,0 +1,9 @@
+mkdir .\addons
+del .\addons\*.*
+copy .\distribution-1.5.0-SNAPSHOT-addons\org.openhab.action.mail-1.5.0-SNAPSHOT.jar .\addons\*.*
+copy .\distribution-1.5.0-SNAPSHOT-addons\org.openhab.binding.exec-1.5.0-SNAPSHOT.jar .\addons\*.*
+copy .\distribution-1.5.0-SNAPSHOT-addons\org.openhab.binding.http-1.5.0-SNAPSHOT.jar .\addons\*.*
+copy .\distribution-1.5.0-SNAPSHOT-addons\org.openhab.binding.proserv-1.5.0-SNAPSHOT.jar .\addons\*.*
+copy .\distribution-1.5.0-SNAPSHOT-addons\org.openhab.persistence.db4o-1.5.0-SNAPSHOT.jar .\addons\*.*
+copy .\distribution-1.5.0-SNAPSHOT-addons\org.openhab.persistence.logging-1.5.0-SNAPSHOT.jar .\addons\*.*
+copy .\distribution-1.5.0-SNAPSHOT-addons\org.openhab.persistence.rrd4j-1.5.0-SNAPSHOT.jar .\addons\*.*
