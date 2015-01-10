6458bc6359006e9f3b5f30080e4dfd4c381cb8b1
jenskastensson <jens@kastensson.se>
2015-01-10 17:30:00 +0100

    -

--- distribution/openhabhome/clean.bat
+++ distribution/openhabhome/clean.bat
@@ -1,13 +1,17 @@
-del .\configurations\items\proserv.items
-del .\configurations\persistence\db4o.persist
-del .\configurations\persistence\rrd4j.persist
-del .\configurations\sitemaps\proserv.sitemap
-del .\configurations\transform\proserv.map
-del .\configurations\ProservCronJobs.ser
-del .\etc\db4o\*.*
-del .\etc\rrd4j\*.*
-del .\etc\habminj\*.*
-del .\logs\*.*
-del .\webapps\static\*.*
+del /Q /S .\configurations\items\proserv.items
+del /Q /S .\configurations\items\proserv-custom.items
+del /Q /S .\configurations\rules\proserv.rules
+del /Q /S .\configurations\persistence\db4o.persist
+del /Q /S .\configurations\persistence\rrd4j.persist
+del /Q /S .\configurations\sitemaps\proserv.sitemap
+del /Q /S .\configurations\sitemaps\proserv-classic.sitemap
+del /Q /S .\configurations\transform\proserv.map
+del /Q /S .\configurations\ProservCronJobs.ser
+del /Q /S .\configurations\openhab.cfg
+del /Q /S .\etc\db4o\*.*
+del /Q /S .\etc\rrd4j\*.*
+del /Q /S .\etc\habmin\charts.xml
+del /Q /S .\logs\*.*
+del /Q /S .\webapps\static\*.*
 rmdir .\webapps\static
-del .\webapps\proserv\scheduler.html
+del /Q /S .\webapps\proserv\scheduler.html
