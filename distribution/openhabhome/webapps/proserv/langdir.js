d938231b6a816398f80c73e4bca43b7399bf809b
jenskastensson <jens@kastensson.se>
2015-03-04 19:07:55 +0100

    update langdir.js

--- distribution/openhabhome/webapps/proserv/langdir.js
+++ distribution/openhabhome/webapps/proserv/langdir.js
@@ -1,6 +1,3 @@
 var langCode='en';
-var dest=window.location.origin+window.location.pathname;
-if(dest.toLowerCase().indexOf("html")==-1){
-  dest=window.location.origin+'/proserv/index.html';
-}
+var dest=window.location.origin+window.location.pathname;if(dest.toLowerCase().indexOf("html")==-1){dest=window.location.origin+'/proserv/index.html';}
 var langcodes=['en','fr','de'];var lang=langCode.toLowerCase();lang=lang.substr(0,2);for(i=langcodes.length-1;i>=0;i--){if(lang==langcodes[i]){dest=dest.substr(0,dest.lastIndexOf('.'))+'-'+lang.substr(0,2)+dest.substr(dest.lastIndexOf('.'));window.location.replace?window.location.replace(dest):window.location=dest;}}
