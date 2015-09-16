d4497f4f831c0699797217cda00cbe07687150ca
jenskastensson <jens@kastensson.se>
2015-09-16 16:29:12 +0200

    1. fix: language redirect didn't work in some old browsers , settings and homepage didn't show at allItemNames 2. style sheet fixes for better display in small devices

--- bundles/binding/org.openhab.binding.proserv/src/main/java/org/openhab/binding/proserv/internal/ProservData.java
+++ bundles/binding/org.openhab.binding.proserv/src/main/java/org/openhab/binding/proserv/internal/ProservData.java
@@ -1767,7 +1767,7 @@ public class ProservData {
 	
 			PrintWriter writer = new PrintWriter(path, "US-ASCII");
 			writer.println("var langCode='" + language + "';");
-			writer.println("var dest=window.location.origin+window.location.pathname;if(dest.toLowerCase().indexOf('html')==-1){dest=window.location.origin+'/proserv/index.html';} var langcodes=['en','fr','de'];var lang=langCode.toLowerCase();lang=lang.substr(0,2);for(i=langcodes.length-1;i>=0;i--){if(lang==langcodes[i]){dest=dest.substr(0,dest.lastIndexOf('.'))+'-'+lang.substr(0,2)+dest.substr(dest.lastIndexOf('.'));window.location.replace?window.location.replace(dest):window.location=dest;}}");
+			writer.println("if(!window.location.origin){window.location.origin=window.location.protocol+'//'+window.location.hostname+(window.location.port?':'+window.location.port:'');}var dest=window.location.origin+window.location.pathname;if(dest.toLowerCase().indexOf('html')==-1){dest=window.location.origin+'/proserv/index.html';}var langcodes=['en','fr','de'];var lang=langCode.toLowerCase();lang=lang.substr(0,2);for(i=langcodes.length-1;i>=0;i--){if(lang==langcodes[i]){dest=dest.substr(0,dest.lastIndexOf('.'))+'-'+lang.substr(0,2)+dest.substr(dest.lastIndexOf('.'));window.location.replace?window.location.replace(dest):window.location=dest;}}");
 			writer.close();
 
 		} catch (IOException e) {
